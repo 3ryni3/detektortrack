c63acd3a6a1c3b433356e0c361a8f6ab0ef46817
3ryni3 <chrzantech@gmail.com>
2026-03-01 20:17:29 +0100

    Add files via upload

--- netlify/functions/geoportal.js
+++ netlify/functions/geoportal.js
@@ -1,6 +1,4 @@
 // netlify/functions/geoportal.js
-// Zaktualizowana wersja – obsługuje zarówno GetParcelById (id=) jak i GetParcelByXY (xy=)
-
 exports.handler = async (event) => {
   const params = event.queryStringParameters || {};
   const { id, xy } = params;
@@ -8,17 +6,12 @@ exports.handler = async (event) => {
   let url;
 
   if (xy) {
-    // Kliknięcie działki na mapie – GetParcelByXY
-    // xy = "lng,lat,4326" (przekazane z frontendu)
-    url = `https://uldk.gugik.gov.pl/?request=GetParcelByXY&xy=${encodeURIComponent(xy)}&result=geom_wkt,teryt,parcel,region,commune,county,voivodeship`;
+    // xy = "lng,lat,4326" – NIE uzywamy encodeURIComponent bo przecinki musza zostac
+    url = `https://uldk.gugik.gov.pl/?request=GetParcelByXY&xy=${xy}&result=geom_wkt,teryt,parcel,region,commune,county,voivodeship`;
   } else if (id) {
-    // Wyszukiwanie po identyfikatorze TERYT – GetParcelById
     url = `https://uldk.gugik.gov.pl/?request=GetParcelById&id=${encodeURIComponent(id)}&result=geom_wkt`;
   } else {
-    return {
-      statusCode: 400,
-      body: 'Brakuje parametru id lub xy',
-    };
+    return { statusCode: 400, body: 'Brakuje parametru id lub xy' };
   }
 
   try {
@@ -33,9 +26,6 @@ exports.handler = async (event) => {
       body: text,
     };
   } catch (e) {
-    return {
-      statusCode: 500,
-      body: 'Błąd proxy: ' + e.message,
-    };
+    return { statusCode: 500, body: 'Blad proxy: ' + e.message };
   }
 };
